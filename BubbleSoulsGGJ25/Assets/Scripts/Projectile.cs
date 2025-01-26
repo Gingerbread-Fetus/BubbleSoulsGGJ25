9cd66e77edeecbf16992cda8faba882fc19205a0
Hayden A. M. <haydenamatthews@gmail.com>
2025-01-26 06:02:40 -0700

    Removed unity version control due to bug

--- BubbleSoulsGGJ25/Assets/Scripts/Projectile.cs
+++ BubbleSoulsGGJ25/Assets/Scripts/Projectile.cs
@@ -16,7 +16,6 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print(rb.velocity);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
